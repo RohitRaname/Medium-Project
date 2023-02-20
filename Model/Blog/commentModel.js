5c9b3550e359d8f12e87d32c49038099df6fbbef
RohitRaname <rohit941012@gmail.com>
2023-02-20 16:13:28 +0530

    create commentModel.js

---
+++ Model/Blog/commentModel.js
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+const mongoose = require('mongoose');
+
+// group by user to limit data loaded when search by user
+// 1000 -> 50
+
+const CommentSchema = new mongoose.Schema({
+  author: {
+    _id: mongoose.Schema.Types.ObjectId,
+    avatar: { type: String, default: 'default.png' },
+    name: String,
+  },
+
+  parentId: mongoose.Schema.Types.ObjectId,
+  ancestorIds: [String],
+
+
+  text: String,
+  active: { type: Boolean, default: false },
+  
+  count: {
+      like: { type: Number, default: 0 },
+      bookmark: { type: Number, default: 0 },
+      replyTo: { type: Number, default: 0 },
+  },
+  
+
+  ts: { type: Date, default: new Date() },
+  
+});
+
+const Comment = mongoose.model('comment', CommentSchema);
+
+module.exports = Comment;
