567147907b9acc4a193d49029b036c40f6b0fdab
RohitRaname <rohit941012@gmail.com>
2023-02-20 15:10:39 +0530

    update historyModel.js

---
+++ Model/historyModel.js
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+const mongoose = require('mongoose');
+
+// group by user to limit data loaded when search by user
+// 1000 -> 50
+
+const HistorySchema = new mongoose.Schema({
+  // user id
+  userId: mongoose.Schema.Types.ObjectId,
+
+  page: { type: Number, default: 0 },
+
+  // this limit if for top level items => review,wishlists
+  limit: { type: Number, default: 100 },
+
+  history: [
+    {
+      authorId: mongoose.Schema.Types.ObjectId,
+      _id: String,
+      title: String,
+      text: String,
+    },
+  ],
+
+  // this will be fast to fill so no need it to be here
+});
+
+HistorySchema.index({ userId: 1, page: 1 });
+
+HistorySchema.index({ userId: 1, 'history._id': 1 });
+
+const History = mongoose.model('history', HistorySchema);
+
+module.exports = History;
