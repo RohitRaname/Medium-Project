e7eabd8d9e711247052b686be155c1419d47142c
RohitRaname <rohit941012@gmail.com>
2023-02-20 15:08:00 +0530

    add count field obj in userModel.js

--- Model/Blog/blogModel.js
+++ Model/Blog/blogModel.js
@@ -22,19 +22,14 @@ const BlogSchema = new mongoose.Schema({
   active: { type: Boolean, default: false },
   ts: { type: Date, default: new Date() },
 
+  count: {
+    like: { type: Number, default: 0 },
+    comment: { type: Number, default: 0 },
+    views: { type: Number, default: 0 },
+  },
+
   // this will be fast to fill so no need it to be here
 });
-
-BlogSchema.index({ userId: 1, page: 1 });
-
-BlogSchema.index({ userId: 1, 'following._id': 1 });
-BlogSchema.index({ userId: 1, 'followers._id': 1 });
-BlogSchema.index({ userId: 1, 'history._id': 1 });
-BlogSchema.index({ userId: 1, 'reviews._id': 1 });
-BlogSchema.index({ userId: 1, 'following._id': 1 });
-BlogSchema.index({ userId: 1, 'cart._id': 1 });
-BlogSchema.index({ userId: 1, 'orders._id': 1 });
-BlogSchema.index({ userId: 1, 'orderItems._id': 1 });
 
 const Blog = mongoose.model('blog', BlogSchema);
 
