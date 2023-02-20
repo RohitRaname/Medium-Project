a7f9aabfcd5a18a4196b5eba9e0e10e29f3d49d2
RohitRaname <rohit941012@gmail.com>
2023-02-20 15:37:12 +0530

    remove followersField from userActivityModel.js and create followingModel.js

---
+++ Model/userActivity/followingModel.js
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+const mongoose = require('mongoose');
+
+// group by user to limit data loaded when search by user
+// 1000 -> 50
+
+const FollowingSchema = new mongoose.Schema({
+  // user id
+  userId: mongoose.Schema.Types.ObjectId,
+
+  page: { type: Number, default: 0 },
+
+  // this limit if for top level items => review,wishlists
+  limit: { type: Number, default: 100 },
+
+  following: [
+    {
+      _id: mongoose.Schema.Types.ObjectId,
+      name: String,
+      avatar: String,
+      bio:String,
+      followers: { type: Number, default: 0 },
+    },
+  ],
+  // this will be fast to fill so no need it to be here
+});
+
+FollowingSchema.index({ userId: 1, page: 1 });
+
+FollowingSchema.index({ userId: 1, 'following._id': 1 });
+
+const Following = mongoose.model('following', FollowingSchema);
+
+module.exports = Following;
