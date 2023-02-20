13662b271dc9458fc650841d0f9f909763bead0e
RohitRaname <rohit941012@gmail.com>
2023-02-20 15:49:03 +0530

    create genreModel.js

---
+++ Model/genreModel.js
@@ -0,0 +1,23 @@
+/* eslint-disable camelcase */
+const mongoose = require('mongoose');
+
+// group by user to limit data loaded when search by user
+// 1000 -> 50
+
+const GenreSchema = new mongoose.Schema({
+
+  genre: String,
+
+
+
+  count: {
+    writers: { type: Number, default: 0 },
+    blogs:{type:Number,default:0},
+  },
+
+  // this will be fast to fill so no need it to be here
+});
+
+const Genre = mongoose.model('genre', GenreSchema);
+
+module.exports = Genre;
