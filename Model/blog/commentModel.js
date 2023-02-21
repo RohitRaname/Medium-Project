8bccf732aac5acc23ab721c7e124d6a90f15e4a9
RohitRaname <rohit941012@gmail.com>
2023-02-21 14:55:09 +0530

    update commentModel from one-one to bucket

--- Model/blog/commentModel.js
+++ Model/blog/commentModel.js
@@ -5,31 +5,46 @@ const mongoose = require('mongoose');
 // 1000 -> 50
 
 const CommentSchema = new mongoose.Schema({
-  author: {
-    _id: mongoose.Schema.Types.ObjectId,
-    avatar: { type: String, default: 'default.png' },
-    name: String,
-  },
+  // treat userId as blogId 
+  userId: mongoose.Schema.Types.ObjectId,
 
-  parentId: mongoose.Schema.Types.ObjectId,
-  ancestorIds: [String],
+  page: { type: Number, default: 0 },
 
-  text: String,
-  active: { type: Boolean, default: false },
-  
-  count: {
-      like: { type: Number, default: 0 },
-      bookmark: { type: Number, default: 0 },
-      replyTo: { type: Number, default: 0 },
-  },
+  // this limit if for top level items => review,wishlists
+  limit: { type: Number, default: 100 },
 
-  ts: { type: Date, default: new Date() },
+  comments: [
+    {
+      author: {
+        _id: mongoose.Schema.Types.ObjectId,
+        avatar: { type: String, default: 'default.png' },
+        name: String,
+      },
 
+      parentId: mongoose.Schema.Types.ObjectId,
+      ancestorIds: [String],
+      
+
+      text: String,
+      active: { type: Boolean, default: true },
+
+      count: {
+        like: { type: Number, default: 0 },
+        bookmark: { type: Number, default: 0 },
+        reply: { type: Number, default: 0 },
+      },
+
+      ts: { type: Date, default: new Date() },
+    },
+  ],
+
+  // this will be fast to fill so no need it to be here
 });
-
 const Comment = mongoose.model('comment', CommentSchema);
+
 CommentSchema.pre(/^find/, function (next) {
   this.find({ active: { $ne: false } });
   next();
 });
+
 module.exports = Comment;
